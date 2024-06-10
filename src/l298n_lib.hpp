af759d2ebc7b1c23385f1bb0ea6e450f898b6a09
PedroAdgs <pedrinkk23@gmail.com>
2024-06-10 09:16:56 -0300

    add comments

--- src/l298n_lib.hpp
+++ src/l298n_lib.hpp
@@ -1,8 +1,8 @@
-#ifndef L298N_H
+#ifndef L298N_H //guarda
 #define L298N_H
 
-//#include "Arduino.h"
-
+#include "Arduino.h"
+//classe criada
 class L298N{
 
     private:
@@ -10,13 +10,12 @@ class L298N{
         int ENB;
         int IN1;
         int IN2;
-        int ENB;
         int IN3;
         int IN4;
 
     public:
-        L298N(int ena, int in1, int in2, int enb, int in3, int in4);
-        void setSpeed(int motor, int speed);
+        L298N(int ena, int in1, int in2, int enb, int in3, int in4); //cria uma instaância de objeto
+        void setSpeed(int motor, int speed); //funções de movimento e velocidade (PWM)
         void forward();
         void backward();
         void left();
